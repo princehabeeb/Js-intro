16a4636fa6b7d0145125254d3d331cc34aaecff9
princehabeeb <habiy3400@gmail.com>
2023-09-27 18:05:15 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,65 @@
+// var age = 18;
+
+
+// if (age <= 17 ){
+//     alert('You are young');
+// }else{
+//     alert('You are matured');
+// }
+
+// var fname = 'Princ';
+
+
+// console.log(typeof true );
+
+
+// const day = 2;
+
+
+
+// if (day == 0){
+//     alert('Today is Sunday');
+// }else if( day == 1){
+//     alert('Today is Monday');
+// }else{
+//     alert('Today is Tuesday');
+// }
+
+// !I love you
+
+// var fname = 'john Doe';
+
+// console.log(fname.length);
+
+
+// if (age != 10) {
+//     console.log('you are good');
+// }else{
+//     console.log('you are not good');
+// }
+// let fname='princ';
+// if (fname == 'prince') {
+//     console.log('you are welcome');   
+// }
+// else if ('!prince'){
+//     console.log('who are you');
+// }
+
+// 
+
+// var age = -12;
+// if (age <= 12) {
+//     prompt('not eligible');
+// } 
+// else if (age >= 12) {
+//     prompt('proceed');
+// }
+// else {
+//     prompt ('enter your age')
+// }
+
+
+var fname = prompt('Your name');
+
+console.log(fname.toUpperCase());
+
